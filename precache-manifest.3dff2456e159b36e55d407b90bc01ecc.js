01c8804ab782b6a3b3f352c694ffbbe3a2ab4e36
m5022 <maksym.yaremishyn@sixt.com>
2020-04-10 17:47:34 +0300

    Updates

---
+++ precache-manifest.3dff2456e159b36e55d407b90bc01ecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854f0b0efb8c75c946b0707196b51ad8",
+    "url": "/react_redux-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "5bbdbd4d33e115e717b0",
+    "url": "/react_redux-tic-tac-toe/static/css/main.015cce64.chunk.css"
+  },
+  {
+    "revision": "b932944fa520161f7d1a",
+    "url": "/react_redux-tic-tac-toe/static/js/2.b0b17989.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_redux-tic-tac-toe/static/js/2.b0b17989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bbdbd4d33e115e717b0",
+    "url": "/react_redux-tic-tac-toe/static/js/main.99f5567a.chunk.js"
+  },
+  {
+    "revision": "a448aa7b3fb6d2021a23",
+    "url": "/react_redux-tic-tac-toe/static/js/runtime-main.4b5d3732.js"
+  }
+]);
